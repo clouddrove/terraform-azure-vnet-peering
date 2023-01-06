9174bf3cfdefb68c50b527403fa857e5b99990f1
nilesh.gadgi <nilesh.gadgi@clouddrove.com>
2023-01-06 14:54:55 +0530

    subnet removed and changed source

--- _example/example.tf
+++ _example/example.tf
@@ -15,8 +15,8 @@ module "resource_group" {
 
 #Vnet
 module "vnet" {
-  source  = "clouddrove/virtual-network/azure"
-  version = "1.0.1"
+  source  = "clouddrove/vnet/azure"
+  version = "1.0.0"
 
   name        = "app"
   environment = "example"
@@ -26,18 +26,12 @@ module "vnet" {
   location            = module.resource_group.resource_group_location
   address_space       = "10.0.0.0/16"
   enable_ddos_pp      = false
-
-  #subnet
-  subnet_names                  = ["subnet1"]
-  subnet_prefixes               = ["10.0.1.0/24"]
-  disable_bgp_route_propagation = false
-
 }
 
 #Vnet
 module "vnet_remote" {
-  source  = "clouddrove/virtual-network/azure"
-  version = "1.0.1"
+  source  = "clouddrove/vnet/azure"
+  version = "1.0.0"
 
   name        = "remote"
   environment = "example"
@@ -47,12 +41,6 @@ module "vnet_remote" {
   location            = module.resource_group.resource_group_location
   address_space       = "20.0.0.0/16"
   enable_ddos_pp      = false
-
-  #subnet
-  subnet_names                  = ["subnet1"]
-  subnet_prefixes               = ["20.0.1.0/24"]
-  disable_bgp_route_propagation = false
-
 }
 
 module "vnet_peering" {
