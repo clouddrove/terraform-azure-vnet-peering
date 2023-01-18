a7c20ae96043af1720675f8b70dce672f8033d9e
13archit <chopra13archit@gmail.com>
2023-01-18 21:37:28 +0530

    Updated 2 way peering for different rg

--- _example/example.tf
+++ _example/example.tf
@@ -2,68 +2,61 @@ provider "azurerm" {
   features {}
 }
 
-module "resource_group_1" {
-  source      = "clouddrove/resource-group/azure"
-  version     = "1.0.1"
+module "resource_group" {
+  source  = "clouddrove/resource-group/azure"
+  version = "1.0.1"
+
   name        = "example"
   environment = "test"
   label_order = ["name", "environment"]
   location    = "North Europe"
 }
 
-module "resource_group_2" {
-  source      = "clouddrove/resource-group/azure"
-  version     = "1.0.1"
-  name        = "example2"
-  environment = "test"
-  label_order = ["name", "environment"]
-  location    = "East US"
-}
-
 
 #Vnet
 module "vnet" {
-  source              = "clouddrove/vnet/azure"
-  version             = "1.0.0"
-  name                = "app"
-  environment         = "example"
-  label_order         = ["name", "environment"]
-  resource_group_name = module.resource_group_1.resource_group_name
-  location            = module.resource_group_1.resource_group_location
+  source  = "clouddrove/vnet/azure"
+  version = "1.0.0"
+
+  name        = "app"
+  environment = "example"
+  label_order = ["name", "environment"]
+
+  resource_group_name = module.resource_group.resource_group_name
+  location            = module.resource_group.resource_group_location
   address_space       = "10.0.0.0/16"
   enable_ddos_pp      = false
 }
 
 #Vnet
 module "vnet_remote" {
-  source      = "clouddrove/vnet/azure"
-  version     = "1.0.0"
+  source  = "clouddrove/vnet/azure"
+  version = "1.0.0"
+
   name        = "remote"
   environment = "example"
   label_order = ["name", "environment"]
 
-  resource_group_name = module.resource_group_2.resource_group_name
-  location            = module.resource_group_2.resource_group_location
+  resource_group_name = module.resource_group.resource_group_name
+  location            = module.resource_group.resource_group_location
   address_space       = "20.0.0.0/16"
   enable_ddos_pp      = false
 }
 
 module "vnet_peering" {
   source     = "../"
-  depends_on = [module.resource_group_1, module.resource_group_2]
+  depends_on = [module.resource_group]
 
-  enabled_peering       = true
-  resource_group_1_name = module.resource_group_1.resource_group_name
-  resource_group_2_name = module.resource_group_2.resource_group_name
+  enabled_peering     = true
+  resource_group_name = module.resource_group.resource_group_name
 
   allow_gateway_transit_vnet1 = false
   use_remote_gateways_vnet1   = false
   allow_gateway_transit_vnet2 = false
   use_remote_gateways_vnet2   = false
-  different_rg                = true
+  vnet_1_name                 = module.vnet.vnet_name[0]
+  vnet_1_id                   = module.vnet.vnet_id[0]
+  vnet_2_name                 = module.vnet_remote.vnet_name[0]
+  vnet_2_id                   = module.vnet_remote.vnet_id[0]
 
-  vnet_1_name = module.vnet.vnet_name[0]
-  vnet_1_id   = module.vnet.vnet_id[0]
-  vnet_2_name = module.vnet_remote.vnet_name[0]
-  vnet_2_id   = module.vnet_remote.vnet_id[0]
 }
--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
+data "azurerm_resource_group" "default" {
+  name = var.resource_group_name
+}
 
+locals {
+  resource_group_name = data.azurerm_resource_group.default.name
+  location            = data.azurerm_resource_group.default.location
+}
+
+# enable global peering between the two virtual network
 resource "azurerm_virtual_network_peering" "peering" {
   count                        = var.enabled_peering ? 1 : 0
   name                         = format("%s-peering-%s", var.vnet_1_name, var.vnet_2_name)
-  resource_group_name          = var.resource_group_1_name
+  resource_group_name          = local.resource_group_name
   virtual_network_name         = var.vnet_1_name
   remote_virtual_network_id    = var.vnet_2_id
   allow_virtual_network_access = var.allow_virtual_network_access
@@ -15,7 +24,7 @@ resource "azurerm_virtual_network_peering" "peering" {
 resource "azurerm_virtual_network_peering" "peering_back" {
   count                        = var.enabled_peering ? 1 : 0
   name                         = format("%s-peering-%s", var.vnet_2_name, var.vnet_1_name)
-  resource_group_name          = var.different_rg ? var.resource_group_2_name : var.resource_group_1_name
+  resource_group_name          = local.resource_group_name
   virtual_network_name         = var.vnet_2_name
   remote_virtual_network_id    = var.vnet_1_id
   allow_virtual_network_access = var.allow_virtual_network_access
