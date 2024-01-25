c76f380ec0d8fb98640866fff800549c303fea51
Rupal Sharma <rupal.sharma@clouddrove.com>
2024-01-25 23:20:13 +0530

    fix:update module version to latest

--- _example/peering_in_same_subscription/example.tf
+++ _example/peering_in_same_subscription/example.tf
@@ -27,25 +27,25 @@ module "resource_group_2" {
 #Vnet
 module "vnet" {
   source  = "clouddrove/vnet/azure"
-  version = "1.0.3"
+  version = "1.0.4"
 
   name                = "app"
   environment         = "test"
   resource_group_name = module.resource_group_1.resource_group_name
   location            = module.resource_group_1.resource_group_location
-  address_space       = "10.0.0.0/16"
+  address_spaces      = ["10.0.0.0/16"]
 }
 
 #Vnet
 module "vnet_remote" {
   source  = "clouddrove/vnet/azure"
-  version = "1.0.3"
+  version = "1.0.4"
 
   name                = "remote"
   environment         = "test"
   resource_group_name = module.resource_group_2.resource_group_name
   location            = module.resource_group_2.resource_group_location
-  address_space       = "20.0.0.0/16"
+  address_spaces       = ["20.0.0.0/16"]
 }
 
 module "vnet_peering" {
@@ -56,9 +56,9 @@ module "vnet_peering" {
   resource_group_2_name = module.resource_group_2.resource_group_name
 
   different_rg = true
-  vnet_1_name  = module.vnet.vnet_name[0]
-  vnet_1_id    = module.vnet.vnet_id[0]
-  vnet_2_name  = module.vnet_remote.vnet_name[0]
-  vnet_2_id    = module.vnet_remote.vnet_id[0]
+  vnet_1_name  = module.vnet.vnet_name
+  vnet_1_id    = module.vnet.vnet_id
+  vnet_2_name  = module.vnet_remote.vnet_name
+  vnet_2_id    = module.vnet_remote.vnet_id
 
 }
