947a69baaa95682d7ed321acbe6508039d99562f
Rupal Sharma <152851038+Rupalgw@users.noreply.github.com>
2024-02-26 15:04:17 +0530

    fix:change in count condition (#27)

--- output.tf
+++ output.tf
@@ -1,29 +1,29 @@
 output "vnet_peer_1_id" {
-  value       = azurerm_virtual_network_peering.peering.*.id
+  value       = try(azurerm_virtual_network_peering.peering[0].id, null)
   description = "The id of the newly created virtual network peering in on first virtual netowork."
 }
 
 output "vnet_peer_2_id" {
-  value       = azurerm_virtual_network_peering.peering_back.*.id
+  value       = try(azurerm_virtual_network_peering.peering_back[0].id, null)
   description = "The id of the newly created virtual network peering in on second virtual netowork."
 }
 
 output "vnet_peer_1_name" {
-  value       = azurerm_virtual_network_peering.peering.*.name
+  value       = try(azurerm_virtual_network_peering.peering[0].name, null)
   description = "The name of the newly created virtual network peering in on first virtual netowork."
 }
 
 output "vnet_peer_2_name" {
-  value       = azurerm_virtual_network_peering.peering_back.*.name
+  value       = try(azurerm_virtual_network_peering.peering_back[0].name, null)
   description = "The name of the newly created virtual network peering in on second virtual netowork."
 }
 
 output "vnet_peer_diff_subs_name" {
-  value       = azurerm_virtual_network_peering.peering_back_diff_subs.*.name
+  value       = try(azurerm_virtual_network_peering.peering_back_diff_subs[0].name, null)
   description = "The name of the newly created virtual network peering in on different subscription virtual netowork."
 }
 
 output "vnet_peer_diff_subs_id" {
-  value       = azurerm_virtual_network_peering.peering_back_diff_subs.*.id
+  value       = try(azurerm_virtual_network_peering.peering_back_diff_subs[0].id, null)
   description = "The id of the newly created virtual network peering in on different subscription virtual netowork."
 }
